e75e7a07865a4e35eee4c4ad98645e5a491cf5f0
xuhao890627 <xuhao890627@163.com>
2017-12-25 15:33:48 +0800

    Update Test.java

--- src/hackathon/Test.java
+++ src/hackathon/Test.java
@@ -6,6 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.Random;
 
 public class Test {
+	
 	public static void main(String[] args) throws Exception {
 		System.out.println("--------------1");
 		exportCsv();
